82f87d8df2f32ac34e440298e9de8483e7314075
jlin <jlin208@illinois.edu>
2021-04-15 17:16:00 -0500

    finished reading data into array

--- main.cpp
+++ main.cpp
@@ -1,18 +1,84 @@
 #include <iostream>
 #include <vector>
 #include "readFromFile.hpp"
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-	std::cout << "Filename: " << argv[1] << std::endl;
-	
-	// convert file to string representation
-	std::cout << "String Representation:" << std::endl;
-	std::cout << file_to_string(argv[1]) << std::endl;
-	
-	// convert file to vector representation
-	std::cout << "Vector Representation:" << std::endl;
-	std::vector<std::string> vectorRepr = file_to_vector(argv[1]);
-	for (auto word : vectorRepr) {
-		std::cout << word << std::endl;
-	}	
+struct Airport{
+	int id;
+	string name;
+	string city;
+	float latitude;
+	float longitude;
+};
+
+// Function to determine if the line of the airport file contains all the information.
+// For it to contain all the information, it must contain 13 commas.
+// This function counts the number of commas in the string and returns true if it is a valid
+// airport. Else, return false.
+bool isValidAirport(string str){
+	int count = 0;
+	for (unsigned i = 0; i < str.size(); i++){
+		if (str[i] == ','){
+			count++;
+		}
+	}
+	if (count == 13){
+		return true;
+	}
+	return false;
+}
+
+// Reads through the string of airport data and intializes the corresponding member variables.
+// Returns a pointer to Airport object.
+Airport * processAirport(string str){
+	stringstream ss(str);
+	vector<string> v;
+	while (ss.good()){
+		string substring;
+		getline(ss, substring, ',');
+		v.push_back(substring);
+	}
+	Airport * airport = new Airport;
+	stringstream temp(v[0]);
+	temp >> airport->id;
+	airport->name = v[1];
+	airport->city = v[2];
+	stringstream temp2(v[6]);
+	temp2 >> airport->latitude;
+	stringstream temp3(v[7]);
+	temp3 >> airport->longitude;
+	return airport;
+}
+
+int main() {
+	vector<Airport *> airports;
+	// Create an input stream
+	ifstream inFile;
+	// Open the data file
+	inFile.open("tests/airports.dat.txt");
+
+	// Check For Error
+	if (inFile.fail()){
+		// Close the file if there is an error
+		cerr << "Error opening file" << endl;
+		exit(1);
+	}
+
+	string temp;
+
+	// Read file line by line until end
+	while (getline(inFile, temp)){
+		if (isValidAirport(temp)){
+			airports.push_back(processAirport(temp));
+		}
+	}
+	cout << airports[1]->id << endl;
+	cout << airports[1]->name << endl;
+	cout << airports[1]->city << endl;
+	cout << airports[1]->latitude << endl;
+	cout << airports[1]->longitude << endl;
+	return 0;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,18 +6,18 @@
 
 
 TEST_CASE("Verify that file_to_string works on a small example") {
-	std::string res = file_to_string("tests/smallSample.txt");
+	std::string res = file_to_string("tests/airport.dat.txt");
 	// always check "expected" == "actual" --> be consistent
 	REQUIRE("hello\nstudents\nof\ncs\n225\n!!!\n" == res);
 }
 
 TEST_CASE("Verify that file_to_vector works on a small example") {
-	std::vector<std::string> res = file_to_vector("tests/smallSample.txt");
+	std::vector<std::string> res = file_to_vector("tests/airport.dat.txt");
 	std::string expected[] = {"hello", "students", "of", "cs", "225", "!!!"};
-	
+
 	REQUIRE(6 == res.size());
 
 	for (int i = 0; i < res.size(); i++) {
 		REQUIRE(expected[i] == res[i]);
 	}
-}		
+}
