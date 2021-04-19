eee695449d322b929cce7fbaf8ec0e6a7c229dd0
jlin <jlin208@illinois.edu>
2021-04-19 00:11:03 -0500

    added function to calculate distance of routes

--- Airports.cpp
+++ Airports.cpp
@@ -1,1 +1,37 @@
 #include "Airports.h"
+
+Airports::Airport* Airports::findAirport(int id){
+  // Calls recursive function
+  return _findAirport(0, airports.size(), id);
+}
+
+Airports::Airport* Airports::_findAirport(int l, int r, int id){
+  if (r >= 1){
+    int mid = l + (r-1)/2;
+    // If the airport is at the middle, return pointer to the airport
+    if (airports[mid].id == id){
+      return &airports[mid];
+    }
+    // If the id of the middle airport is larger, search left subarray
+    if (airports[mid].id > id){
+      return _findAirport(l, mid-1, id);
+    }
+    // Else the id of the middle airport is smaller, search right subarray
+    return _findAirport(mid+1, r, id);
+  }
+  // The airport is not present
+  return NULL;
+}
+
+void Airports::calculateDistance(Route* route){
+  // Find the source and destination airports
+  Airports::Airport * sourceAirport = findAirport(route->sourceAirportId);
+  Airports::Airport * destinationAirport = findAirport(route->destinationAirportId);
+  // Get their coordinates
+  double sourceLat = sourceAirport->latitude;
+  double sourceLong = sourceAirport->longitude;
+  double destinationLat = destinationAirport->latitude;
+  double destinationLong = destinationAirport->longitude;
+  // Calculate the euclidean distance between the two airports
+  route->distance = std::sqrt(std::pow(sourceLat-destinationLat, 2) + std::pow(sourceLong-destinationLong, 2));
+}
--- Airports.h
+++ Airports.h
@@ -1,20 +1,44 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cmath>
 
 struct Airports{
+
+	struct Route{
+		int sourceAirportId;
+	  int destinationAirportId;
+		// Euclidean distance between source and destination airports
+	  double distance;
+	};
+
 	struct Airport{
 		int id;
 		std::string name;
 		std::string city;
 		float latitude;
 		float longitude;
+		// Vector of routes that connect to this airport
+		std::vector<Route> routes;
 	};
 
-	struct Route{
-		int sourceAirportId;
-	  int destinationAirportId;
-	  double distance;
-	};
-	std::vector<Route> routes;
+	// Vector of airports
+	std::vector<Airport> airports;
+
+	// Sets the distance between the source and destination airports of the route
+	// @param pointer to the route
+	// @return nothing
+	void calculateDistance(Route* route);
+
+	// Returns a pointer to the airport with id
+	// @param id of the airport
+	// @return pointer to the airport if found; else NULL
+	Airport* findAirport(int id);
+
+	// Helper function that uses binary search to find the airport with id
+	// Can use binary search since the airport data is sorted by increasing id
+	// @param left edge of subarray, right edge of subarray, id of airport
+	// @return pointer to airport if found; else NULL
+	Airport* _findAirport(int l, int r, int id);
+
 };
