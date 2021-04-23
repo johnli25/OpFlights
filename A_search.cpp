3e6e6cc2f3ff28d047a77229e11fcdd618ccfac8
Ben Thuma <bthuma2@illinois.edu>
2021-04-23 00:42:42 -0500

    Got started on the A star search, this algorithm is a bit complex so I spent most of the time just learning about it.  Linked a few of the videos I watched and there is some basic code with comments if anyone wants to continue work on it.

---
+++ A_search.cpp
@@ -0,0 +1,32 @@
+#include "A_search.h"
+
+//https://www.youtube.com/watch?v=ySN5Wnu88nE - this ones really good 
+//https://www.youtube.com/watch?v=6TsL96NAZCo
+
+
+A_search(queue<airport> airports, airport start, airport end){
+  queue<int> weight;
+  return A_search(weight, airports, start, end);
+}
+
+
+//queue will hold the order
+//weight should be pushed and popped at the same time
+//and will hold weight/cost of an airport
+
+A_search(queue<int> weight, queue<airport> queue, airport* curr, airport* end){ //recursive
+  int cost = 0;//edge length + distance to goal 
+  for(unsigned long i = 0; i < curr->routes.size(); i++){
+    airport* currRoute = findAirport(curr.routes[i].destinationAirportId); 
+    // Get their coordinates, taken from calculateDistance
+    double currLat = currAirport->latitude;
+    double currLong = currAirport->longitude;
+    double endLat = end->latitude;
+    double endLong = end->longitude;
+    // Calculate the euclidean distance between the two airports
+    int heuristic = distance = std::sqrt(std::pow(currLat-endLat, 2) + std::pow(currLong-endLong, 2));
+    cost = start.routes[i].distance + heuristic;
+    //not sure how pushing works yet so if...? ...cost >=...
+    //then push to airport to queue and push cost to weight
+  }
+}
---
+++ A_search.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Airports.h"
+
+A_search(queue<airport> airports, airport start, airport end);
+
+A_search(queue<int> weight, queue<airport> queue, airport current, airport end);
