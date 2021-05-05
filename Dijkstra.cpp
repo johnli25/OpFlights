960708a2cfa4c7395e3f56c69a3b0e10ddb22696
Katrina Pang <kpang3@illinois.edu>
2021-05-05 13:28:43 -0500

    working on dijkstras

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,4 +1,6 @@
 #include "Dijkstra.h"
+#include "Heap.h"
+#include <cfloat>
 
 Dijkstra::Dijkstra(){
   graph = NULL;
@@ -15,6 +17,15 @@ Dijkstra::Dijkstra(Graph * airports, int sourceId){
 // work on this
 void Dijkstra::runDijkstra(){
   // reset distance, heuristic, predecessorID, and visited to 0 for each airport in the graph first
+  // use graph as input to dijkstras 
+  // invalid airports have ID of 0
+  // figure out which are incoming vs outgoing
+  // When setting the distance to infinity, use `DBL_MAX` from `#include <cfloat>`
+  // line 20 --- to update distance: must get airport first, update distance 1st, pass in to update elem in heap: updateElem(airport)
+  // A route is inbound if the current airportID is the destinationID
+  // A route is outbound if the current airportID is the sourceID
+  
+
 }
 
 // work on this
