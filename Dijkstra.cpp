b9da81a780621d1920a7b6dba918060d6798e623
Katrina Pang <kpang3@illinois.edu>
2021-05-06 21:59:51 -0500

    dijkstras

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -18,33 +18,59 @@ Dijkstra::Dijkstra(Graph * airports, int sourceId){
 void Dijkstra::runDijkstra(){
   // reset distance, heuristic, predecessorID, and visited to 0 for each airport in the graph first
   // use graph as input to dijkstras 
-  // invalid airports have ID of 0
-  // figure out which are incoming vs outgoing
-  // When setting the distance to infinity, use `DBL_MAX` from `#include <cfloat>`
   // line 20 --- to update distance: must get airport first, update distance 1st, pass in to update elem in heap: updateElem(airport)
-  // A route is inbound if the current airportID is the destinationID
-  // A route is outbound if the current airportID is the sourceID
 
   //initialize predecessors and distances
-  double dist[];
-  int pred[];
+  int num_airports = graph->airports.size();
   //foreach (Vertex v: G)
-  for (unsigned long i = 0; i < sourceAirportId.routes.size(); i++){
-    dist[i] = DBL_MAX;
-    pred[i] = NULL;
+  Graph::Airport * airport;
+  for (int i = 0; i < num_airports; i++){
+    airport = graph->findAirport(i);
+    airport->distance = DBL_MAX;
+    airport->predecessorId = 0;
+    airport->heuristic = 0;
   }
-  dist[sourceAirportId] = 0;
-
-  //build queue
-  Heap queue();
-  
-
+  graph->findAirport(sourceAirportId)->distance = 0;
+  // Build priority queue heap
+  Heap queue(graph->airports); // Airports vector makes up vertices
+  // Repeat until all vertices have been visited
+  for (int i = 0; i < num_airports; i++){
+    Graph::Airport airport;
+    // Take current min distance airport
+    airport = queue.pop();
+    // invalid airports have ID of 0!!!
+    if (airport.id == 0){
+      continue;
+    }
+    // Examine neighbors: Any edge leaving the airport leads to a neighbor
+    for (int j = 0; j < num_airports; j++){
+      Graph::Airport * neighbor = graph->findAirport(j);
+      if (i == j || neighbor->id == 0){
+        continue;
+      }
+      // A route is inbound if the current airportID is the destinationID
+      // A route is outbound if the current airportID is the sourceID
+      //for (rt = airport.routes.begin(); rt < airport.routes.end(); rt++){
+      for (int k = 0; k < (int)airport.routes.size(); k++){
+        Graph::Route* rt = airport.routes[k];
+        if (rt->destinationAirportId == j){
+          if (airport.distance + rt->distance < neighbor->distance){
+            neighbor->distance = airport.distance + rt->distance;
+            neighbor->predecessorId = i;
+            neighbor->heuristic = 1;
+            queue.updateElem(*neighbor);
+          }
+        }
+      }
+    }
+  }
 }
 
 // work on this
 std::vector<int> findShortestPath(int destinationAirportId){
   std::vector<int> path;
-  destinationAirportId = 0; //filler value to avoid warning for now
+  int destination = destinationAirportId;
+  
   return path;
 }
 
