37a3de114599222bcb4745ddb6262fc31ad8021d
jlin <jlin208@illinois.edu>
2021-05-10 15:50:06 -0500

    main

--- main.cpp
+++ main.cpp
@@ -36,12 +36,18 @@ Graph * enterData(){
 void dijkstra(Graph * graph){
 	bool done = 0;
 	bool ranDijk = 0;
+	std::string startingAirport;
 	while (done != 1){
-		std::string startingAirport;
-		// Get the name of the airport to start Dijkstra at
-		cout<<"Enter the starting airport name: ";
-		cin>>std::ws;
-		std::getline(std::cin, startingAirport);
+		if (startingAirport == ""){
+			// Get the name of the airport to start Dijkstra at
+			cout<<"Enter the starting airport name or EXIT to go back: ";
+			cin>>std::ws;
+			std::getline(std::cin, startingAirport);
+			if (startingAirport == "EXIT"){
+				// Exits while (done != 1)
+				break;
+			}
+		}
 		// Find the id corresponding to the starting airport
 		int startingAirportId = graph->findAirportID(startingAirport);
 		if (startingAirportId == 0){
@@ -65,6 +71,8 @@ void dijkstra(Graph * graph){
 		if (choice == 0){
 			// Need to rerun it using the new starting airport
 			ranDijk = 0;
+			// Reset the starting airport
+			startingAirport = "";
 			// Goes back to while(done != 1)
 			continue;
 		}else if (choice == 1){
@@ -101,7 +109,7 @@ void dijkstra(Graph * graph){
 			for (size_t i = 0; i < path.size(); i++){
 				shortest.push_back(graph->airports[path[i]].name);
 			}
-			cout<<"The shortest path from "<<startingAirport<<" to "<<destinationAirport<<" is "<<endl;
+			cout<<"The shortest path from "<<startingAirport<<" to "<<destinationAirport<<" is:";
 			for (size_t i = 0; i < shortest.size(); i++){
 				cout<<shortest[i]<<endl;
 			}
@@ -137,117 +145,28 @@ int main() {
 			// Goes back to while (done != 1)
 			continue;
 		}else if (choice == 1){
-
+			// DO THIS!!!!!!!!!
+			// Runs BFS iterator
+			// Make a new function; asks the user to input starting airport
+			// then asks if the user wants to see current airport, next airport, etc. using the BFS
+			//bfs(graph);
 		}else if (choice == 2){
 			dijkstra(graph);
 		}else if (choice == 3){
-
+			// DO THIS!!!!!!!!
+			// Runs A*Search
+			// Make a new function; asks the user to input starting airport and destination airport
+			// Add other prompts similar to Dijkstra's above
+			// a_search(grapj);
 		}else{
 			done = 1;
 			// Exits while (done != 1)
 			break;
 		}
 	}
+	// Delete the graph when done
 	if (graph != NULL){
 		delete graph;
 	}
-/*
-	std::vector<Graph::Airport> airports;
-	airports = file_to_Airport("tests/airportsExample.dat.txt");
-	std::vector<Graph::Route> routes;
-	routes = file_to_Route("tests/routesExample.dat.txt");
-	Graph airportss(airports, routes);
-	//Dijkstra(&airportss, 1);
-
-	//testing A_search
-
-        A_search aStar = A_search(&airportss, 1);
-	std::vector<int> traversal = aStar.runA_search(4029);
-	for(size_t i=0; i < traversal.size(); i++){
-	  std::cout << traversal.at(i) << ' ';
-        }
-				*/
-	/*
-	std::vector<Graph::Airport> airports;
-	airports = file_to_Airport("tests/airportsMain.dat.txt");
-	std::vector<Graph::Route> routes;
-	routes = file_to_Route("tests/routes.dat.txt");
-	std::cout<<airports[0].id<<std::endl;
-	std::cout<<airports[0].name<<std::endl;
-	std::cout<<airports[0].city<<std::endl;
-	std::cout<<airports[0].latitude<<std::endl;
-	std::cout<<airports[0].longitude<<std::endl;
-	std::cout<<airports[1].id<<std::endl;
-	std::cout<<airports[1].name<<std::endl;
-	std::cout<<airports[1].city<<std::endl;
-	std::cout<<airports[1].latitude<<std::endl;
-	std::cout<<airports[1].longitude<<std::endl;
-	std::cout<<routes[0].sourceAirportId<<std::endl;
-	std::cout<<routes[0].destinationAirportId<<std::endl;
-	std::cout<<routes[1].sourceAirportId<<std::endl;
-	std::cout<<routes[1].destinationAirportId<<std::endl;
-	Graph airportss(airports, routes);
-	Graph airportss2(airportss);
-	Graph::Airport ap11 = airportss.airports[1];
-	Graph::Route * ro110 = ap11.routes[0];
-	Graph::Route * ro111 = ap11.routes[1];
-	Graph::Airport ap12 = airportss.airports[2];
-	Graph::Airport ap21 = airportss2.airports[1];
-	Graph::Route * ro210 = ap21.routes[0];
-	Graph::Route * ro211 = ap21.routes[1];
-	Graph::Airport ap22 = airportss2.airports[1];
-	std::cout<<airportss.allRoutes->sourceAirportId<<std::endl;
-	std::cout<<airportss.allRoutes->destinationAirportId<<std::endl;
-	std::cout<<airportss.allRoutes->next->sourceAirportId<<std::endl;
-	std::cout<<airportss.allRoutes->next->destinationAirportId<<std::endl;
-	std::cout<<airportss2.allRoutes->sourceAirportId<<std::endl;
-	std::cout<<airportss2.allRoutes->destinationAirportId<<std::endl;
-	std::cout<<airportss2.allRoutes->next->sourceAirportId<<std::endl;
-	std::cout<<airportss2.allRoutes->next->destinationAirportId<<std::endl;
-	Graph::Airport* air = airportss.findAirport(1);
-	std::cout<<air->id<<std::endl;
-	air = airportss2.findAirport(1);
-	std::cout<<air->id<<std::endl;
-	air = airportss.findAirport(2);
-	std::cout<<air->id<<std::endl;
-	air = airportss2.findAirport(2);
-	std::cout<<air->id<<std::endl;
-	*/
-
-
-	// Testing heap
-	/*
-	std::vector<Graph::Airport> airports;
-	airports = file_to_Airport("tests/airportsMain2.dat.txt");
-	std::vector<Graph::Route> routes;
-	routes = file_to_Route("tests/routes.dat.txt");
-	Graph ap(airports, routes);
-	for (int i = 0; i < ap.airports.size(); i++){
-		if (ap.airports[i].id != 0){
-			ap.airports[i].distance = DBL_MAX;
-			ap.airports[i].heuristic = i-2;
-		}
-	}
-	Heap heap(ap.airports);
-	Graph::Airport a1;
-	a1.id = 5;
-	a1.distance = 7;
-	a1.heuristic = 3;
-	heap.push(a1);
-	Graph::Airport a2;
-	a2.id = 6;
-	a2.distance = 5;
-	a2.heuristic = 4;
-	heap.push(a2);
-	Graph::Airport air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	air = heap.pop();
-	*/
 	return 0;
 }
