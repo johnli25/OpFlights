472e7471b7bae80449eb290aff25ae415194ec16
Ben Thuma <bthuma2@illinois.edu>
2021-05-12 14:16:40 -0500

    Cleaned up A_search c++ and header files.  Added comments to the header file.

--- A_search.h
+++ A_search.h
@@ -6,21 +6,38 @@
 
 class A_search{
  public:
+  // Default constructor
   A_search();
+
+  // Custom constructor
+  // @param pointer to airports data graph, id of airport where you start the search from
   A_search(Graph * airports, int sourceId);
+
+  // Runs the A*search algorithm taking the id of the destination airport as 
+  // a parameter and outputting the full path from source to destination in a 
+  // vector, only the destination is printed if no path exists
+  // @param id of airport to end the search
   std::vector<int> runA_search(int destinationAirportId);
+
+  // Sets the graph that Dijkstra is run on
+  // @param graph of airports
   void setGraph(Graph * airports);
+
+  // Sets the starting airport
+  // @param id of starting airport
   void setStart(int sourceId);
+
+  // Returns a pointer to the input graph
+  // @param Dijkstra object
   Graph * getGraph();
+
  private:
+  //  Private helper function used to calculate heuristic values
+  //  @param pointer to source and destination airport
   double distance(Graph::Airport * sourceAirport, Graph::Airport * destinationAirport);
+
+  // Pointer to airports graph
   Graph * graph;
+  // Starting airportID
   int sourceAirportId;
 };
-//comments needed, adjusted to be consistent with dijkstra
-
-
-
-  /*A_search(vector<Airports::Airport*> airports, Airports::Airport* source, Airports::Airport* destination);
-
-    A_search(queue<int> weight, queue<Airports::Airport> queue, Airports::Airport current, Airports::Airport end);*/
